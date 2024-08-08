3491f87b59a0757e907db88043988856d7485723
Timo P. Becker <3785547+timopb@users.noreply.github.com>
2024-08-09 01:09:40 +0200

    Added windows start script, fixed phi prompt

--- app/prompt_generators/phi.py
+++ app/prompt_generators/phi.py
@@ -12,14 +12,14 @@ def build_phi_prompt(conf, query,history,user_name):
     """
 
     prompt = "<|system|>\n"
-    prompt += conf.SYSTEM_MESSAGE + "<|end|>\n"
+    prompt += conf.SYSTEM_MESSAGE + "\n"
 
     # Add warmup message to kick off chat into the right direction unless
     # we have a decent amount of chat history
     if hasattr(conf,'WARMUP') and (len(history) <= conf.HISTORY_COUNT):
         prompt += "\n" + conf.WARMUP
 
-    prompt += "\n\nGenerate " + conf.BOT_NAME + "'s response to the following conversation. Enclose emotes in asterisks. Always stay in character.\n"
+    prompt += "Generate " + conf.BOT_NAME + "'s response to the following conversation. Enclose emotes in asterisks. Always stay in character.<|end|>\n"
 
     if not history == []:
         # build prompt from history + query with
---
+++ app/start.ps1
@@ -0,0 +1,2 @@
+$env:MODELS_FOLDER="c:\models"  
+uvicorn main:app --reload --host 0.0.0.0 --port 8123
